7f6f97a2e419065236b5ab2203627aaeb2c5eb41
meKOTI <114566769+meKOTI@users.noreply.github.com>
2025-01-25 20:59:26 +0100

    mucha i inne

--- Assets/Scripts/Enemy/EnemyMovementMucha.cs
+++ Assets/Scripts/Enemy/EnemyMovementMucha.cs
@@ -4,54 +4,40 @@ using UnityEngine;
 
 public class EnemyMovementMucha : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
     private float speed = 3f;
 
     private float distance;
 
-    private PlayerMovement playerMovement;
-    private PlayerHealth playerHealth;
-
-    private int damage = 1;
+    public EnemyKnockBack enemyKnockBack;
+    public EnemyRange enemyRange;
 
     void Start()
     {
         player = GameObject.Find("Player");
-        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
-        playerHealth = GameObject.Find("Player").GetComponent<PlayerHealth>();
     }
 
     void Update()
     {
-
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.MovePosition(transform.position + Vector3.forward * Time.deltaTime);
-        distance = Vector3.Distance(transform.position, player.transform.position);
-        Vector3 direction = player.transform.position - transform.position;
-        Vector3 localScale = transform.localScale;
-        if (direction.x < 0) localScale.x = Mathf.Abs(localScale.x);
-        else localScale.x = Mathf.Abs(localScale.x) * -1;
-        transform.localScale = localScale;
-
-        transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-    }
-
-    private void OnTriggerStay2D(Collider2D collider)
-    {
-        if (playerHealth.ImmunityCounter <= 0)
+        rb.gravityScale = 0;
+        if (enemyKnockBack.KnockbackCounter <= 0 && enemyRange.isInRange)
         {
-            if (collider.CompareTag("Player"))
-            {
-                playerHealth.TakeDamage(damage);
-                if (collider.transform.position.x <= transform.position.x)
-                {
-                    playerMovement.KnockFromRight = true;
-                }
-                else
-                {
-                    playerMovement.KnockFromRight = false;
-                }
-            }
+
+            rb.MovePosition(transform.position + Vector3.forward * Time.deltaTime);
+            distance = Vector3.Distance(transform.position, player.transform.position);
+
+            Vector3 direction = player.transform.position - transform.position;
+
+            Vector3 localScale = transform.localScale;
+            if (direction.x < 0) localScale.x = Mathf.Abs(localScale.x);
+            else localScale.x = Mathf.Abs(localScale.x) * -1;
+            transform.localScale = localScale;
+
+            transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+        else if(!enemyRange.isInRange) {
+            rb.velocity = Vector2.zero;
         }
     }
 }
